30a9d74e05abb6ae259491e97b4473211281731e
luozhongxue <249590716@qq.com>
2019-10-09 12:42:02 +0800

    修改多余的类

---
+++ src/main/java/com/goodcub/vci/controller/site/IndexController.java
@@ -0,0 +1,14 @@
+package com.goodcub.vci.controller.site;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * @Author Luo.z.x
+ * @Description: TODO
+ * @Date 2019/10/9
+ * @Version V1.0
+ **/
+public class IndexController {
+    protected Log log = LogFactory.getLog(this.getClass());
+}
