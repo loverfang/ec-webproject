dfee0b98478c9aac42b6c12ea51b504ba1a94cc8
loverfang <249590716@qq.com>
2019-11-01 07:38:54 +0800

    格式调整了一下

--- src/main/java/com/goodcub/config/WebMvcConfig.java
+++ src/main/java/com/goodcub/config/WebMvcConfig.java
@@ -35,7 +35,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.viewResolver(resolver);
     }
 
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // 设置文件上传的文件不拦截
